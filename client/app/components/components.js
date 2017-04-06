7c21decdc49fdfc028e79a49361fc48a5b3e8599
Nicholas Morrell <nicholas.morrell@saltermitchell.com>
2017-04-05 23:46:53 -0400

    removed components from starter

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,12 +1,10 @@
 import angular from 'angular';
-import Home from './home/home';
 import About from './about/about';
 import Contacts from './contacts/contacts';
 import ContactForm from './contactForm/contactForm';
 import ContactListItem from './contactListItem/contactListItem';
 
 let componentModule = angular.module('app.components', [
-  Home,
   About, 
   Contacts, 
   ContactForm, 
