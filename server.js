b0aac619f15b9f8e79337ab1ad18af1e9b70f3bb
Nicholas Morrell <nicholas.morrell@saltermitchell.com>
2017-04-07 00:37:42 -0400

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let port = process.env.PORT || 3005;
 app.set('port', port); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html')); 
+    res.sendFile(path.join(__dirname, 'index.html')); 
 }); 
 const server = app.listen(port, () => {
     console.log('Listening on' + port); 
