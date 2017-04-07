f89661cfc73867716e32050babf2a6e840486cff
Nicholas Morrell <nicholas.morrell@saltermitchell.com>
2017-04-07 00:38:59 -0400

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 "use strict" 
 
 const express = require('express'); 
-const path = require('path');
+
 var app = express(); 
 app.use(express.static('dist')); 
 let port = process.env.PORT || 3005; 
@@ -9,7 +9,7 @@ let port = process.env.PORT || 3005;
 app.set('port', port); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); 
+    res.sendFile('index.html'); 
 }); 
 const server = app.listen(port, () => {
     console.log('Listening on' + port); 
